f2b79f40e31acf7d25a8490e568b2abf27f1cdb4
Chenglong <mestway@gmail.com>
2018-07-05 09:05:41 +0100

    update scripts

--- concolic/janala2/process.py
+++ concolic/janala2/process.py
@@ -1,33 +1,54 @@
 import os
+import sys
 
 eq1a = []
 eq1b = []
 eq2a = []
 eq2b = []
 
+entries = []
+
 if __name__ == '__main__':
-    with open("time.txt") as f:
+    
+    datafile = sys.argv[1]
+
+    with open(datafile) as f:
         lines = f.readlines()
-        current = eq1a
+        current = []
         for l in lines:
-            if "equiv1b" in l:
-                current = eq1b
-            if "equiv2a" in l:
-                current = eq2a
-            if "equiv2b" in l:
-                current = eq2b
+            if len(current) == 10:
+                entries.append(current)
+                current = []
             if l.startswith("real"):
                 t_str = l[4:].strip()
                 minute = float(t_str[0])
                 sec = float(t_str[2:-1])
                 current.append(round(minute * 60 + sec, 2))
 
-        for i in range(len(eq1a)):
-            print("{{ \"table_size (#tuples)\" : {}, \"time\": {}, \"Refinement?\" : \"true\" }},".format((i + 1) * 10 , eq2a[i]))
-            print("{{ \"table_size (#tuples)\" : {}, \"time\": {}, \"Refinement?\" : \"false\"  }},".format((i + 1) * 10, eq2b[i]))
+        eq1a = entries[0]
+        eq1b = entries[1]
+        eq2a = entries[2]
+        eq2b = entries[3]
+
+        eq1_str =  ",\n".join(["{{ \"table_size (#tuples)\" : {}, \"time\": {}, \"Refinement?\" : \"true\" }}".format((i + 1) * 10 , eq1a[i]) for i in range(len(eq1a))])
+        eq1_str += "\n" + ",\n".join(["{{ \"table_size (#tuples)\" : {}, \"time\": {}, \"Refinement?\" : \"false\" }}".format((i + 1) * 10 , eq1b[i]) for i in range(len(eq1b))])
+        eq1_str = "[" + eq1_str + "]"
+
+        print(eq1_str)
+
+        eq2_str =  ",\n".join(["{{ \"table_size (#tuples)\" : {}, \"time\": {}, \"Refinement?\" : \"true\" }}".format((i + 1) * 10 , eq2a[i]) for i in range(len(eq2a))])
+        eq2_str += "\n" + ",\n".join(["{{ \"table_size (#tuples)\" : {}, \"time\": {}, \"Refinement?\" : \"false\" }}".format((i + 1) * 10 , eq2b[i]) for i in range(len(eq2b))])
+        eq2_str = "[" + eq2_str + "]"
+
+        print("")
+        print(eq2_str)
+
+        #for i in range(len(eq1a)):
+        #    print("{{ \"table_size (#tuples)\" : {}, \"time\": {}, \"Refinement?\" : \"true\" }},".format((i + 1) * 10 , eq2a[i]))
+        #    print("{{ \"table_size (#tuples)\" : {}, \"time\": {}, \"Refinement?\" : \"false\"  }},".format((i + 1) * 10, eq2b[i]))
             #print("{{ \"size\" : {}, \"time\": {}  }},".format(i * 10, eq2a[i]))
             #print("{{ \"size\" : {}, \"time\": {}  }},".format(i * 10, eq2b[i]))
-        print(eq1a)
-        print(eq1b)
-        print(eq2a)
-        print(eq2b)
+        #print(eq1a)
+        #print(eq1b)
+        #print(eq2a)
+        #print(eq2b)
--- concolic/janala2/run_test.sh
+++ concolic/janala2/run_test.sh
@@ -1,55 +1,95 @@
 touch time.txt
 rm time.txt
 
->&2 echo "start"
->&2 echo "equiv1a"
+{ echo "start" ; } 2>> time.txt
+{ echo "equiv1a" ; } 2>> time.txt
 
+echo "Running equiv1a_20_10"
 { time python concolic.py --coverage 20 custom.EquivTest1a 10 > equiv1a_20_10.log ; } 2>> time.txt
+echo "Running equiv1a_20_20"
 { time python concolic.py --coverage 20 custom.EquivTest1a 20 > equiv1a_20_20.log ; } 2>> time.txt
+echo "Running equiv1a_20_30"
 { time python concolic.py --coverage 20 custom.EquivTest1a 30 > equiv1a_20_30.log ; } 2>> time.txt
+echo "Running equiv1a_20_40"
 { time python concolic.py --coverage 20 custom.EquivTest1a 40 > equiv1a_20_40.log ; } 2>> time.txt
+echo "Running equiv1a_20_50"
 { time python concolic.py --coverage 20 custom.EquivTest1a 50 > equiv1a_20_50.log ; } 2>> time.txt
+echo "Running equiv1a_20_60"
 { time python concolic.py --coverage 20 custom.EquivTest1a 60 > equiv1a_20_60.log ; } 2>> time.txt
+echo "Running equiv1a_20_70"
 { time python concolic.py --coverage 20 custom.EquivTest1a 70 > equiv1a_20_70.log ; } 2>> time.txt
+echo "Running equiv1a_20_80"
 { time python concolic.py --coverage 20 custom.EquivTest1a 80 > equiv1a_20_80.log ; } 2>> time.txt
+echo "Running equiv1a_20_90"
 { time python concolic.py --coverage 20 custom.EquivTest1a 90 > equiv1a_20_90.log ; } 2>> time.txt
+echo "Running equiv1a_20_100"
 { time python concolic.py --coverage 20 custom.EquivTest1a 100 > equiv1a_20_100.log ; } 2>> time.txt
 
->&2 echo "equiv1b"
+{ echo "equiv1b" ; } 2>> time.txt
 
+echo "Running equiv1b_20_10"
 { time python concolic.py --coverage 20 custom.EquivTest1b 10 > equiv1b_20_10.log ; } 2>> time.txt
+echo "Running equiv1b_20_20"
 { time python concolic.py --coverage 20 custom.EquivTest1b 20 > equiv1b_20_20.log ; } 2>> time.txt
+echo "Running equiv1b_20_30"
 { time python concolic.py --coverage 20 custom.EquivTest1b 30 > equiv1b_20_30.log ; } 2>> time.txt
+echo "Running equiv1b_20_40"
 { time python concolic.py --coverage 20 custom.EquivTest1b 40 > equiv1b_20_40.log ; } 2>> time.txt
+echo "Running equiv1b_20_50"
 { time python concolic.py --coverage 20 custom.EquivTest1b 50 > equiv1b_20_50.log ; } 2>> time.txt
+echo "Running equiv1b_20_60"
 { time python concolic.py --coverage 20 custom.EquivTest1b 60 > equiv1b_20_60.log ; } 2>> time.txt
+echo "Running equiv1b_20_70"
 { time python concolic.py --coverage 20 custom.EquivTest1b 70 > equiv1b_20_70.log ; } 2>> time.txt
+echo "Running equiv1b_20_80"
 { time python concolic.py --coverage 20 custom.EquivTest1b 80 > equiv1b_20_80.log ; } 2>> time.txt
+echo "Running equiv1b_20_90"
 { time python concolic.py --coverage 20 custom.EquivTest1b 90 > equiv1b_20_90.log ; } 2>> time.txt
+echo "Running equiv1b_20_100"
 { time python concolic.py --coverage 20 custom.EquivTest1b 100 > equiv1b_20_100.log ; } 2>> time.txt
 
->&2 echo "equiv2a"
+{ echo "equiv2a" ; } 2>> time.txt
 
+echo "Running equiv2a_20_10"
 { time python concolic.py --coverage 20 custom.EquivTest2a 10 > equiv2a_20_10.log ; } 2>> time.txt
+echo "Running equiv2a_20_20"
 { time python concolic.py --coverage 20 custom.EquivTest2a 20 > equiv2a_20_20.log ; } 2>> time.txt
+echo "Running equiv2a_20_30"
 { time python concolic.py --coverage 20 custom.EquivTest2a 30 > equiv2a_20_30.log ; } 2>> time.txt
+echo "Running equiv2a_20_40"
 { time python concolic.py --coverage 20 custom.EquivTest2a 40 > equiv2a_20_40.log ; } 2>> time.txt
+echo "Running equiv2a_20_50"
 { time python concolic.py --coverage 20 custom.EquivTest2a 50 > equiv2a_20_50.log ; } 2>> time.txt
+echo "Running equiv2a_20_60"
 { time python concolic.py --coverage 20 custom.EquivTest2a 60 > equiv2a_20_60.log ; } 2>> time.txt
+echo "Running equiv2a_20_70"
 { time python concolic.py --coverage 20 custom.EquivTest2a 70 > equiv2a_20_70.log ; } 2>> time.txt
+echo "Running equiv2a_20_80"
 { time python concolic.py --coverage 20 custom.EquivTest2a 80 > equiv2a_20_80.log ; } 2>> time.txt
+echo "Running equiv2a_20_90"
 { time python concolic.py --coverage 20 custom.EquivTest2a 90 > equiv2a_20_90.log ; } 2>> time.txt
+echo "Running equiv2a_20_100"
 { time python concolic.py --coverage 20 custom.EquivTest2a 100 > equiv2a_20_100.log ; } 2>> time.txt
 
->&2 echo "equiv2b"
+{ echo "equiv2b" ; } 2>> time.txt
 
+echo "Running equiv2b_20_10"
 { time python concolic.py --coverage 20 custom.EquivTest2b 10 > equiv2b_20_10.log ; } 2>> time.txt
+echo "Running equiv2b_20_20"
 { time python concolic.py --coverage 20 custom.EquivTest2b 20 > equiv2b_20_20.log ; } 2>> time.txt
+echo "Running equiv2b_20_30"
 { time python concolic.py --coverage 20 custom.EquivTest2b 30 > equiv2b_20_30.log ; } 2>> time.txt
+echo "Running equiv2b_20_40"
 { time python concolic.py --coverage 20 custom.EquivTest2b 40 > equiv2b_20_40.log ; } 2>> time.txt
+echo "Running equiv2b_20_50"
 { time python concolic.py --coverage 20 custom.EquivTest2b 50 > equiv2b_20_50.log ; } 2>> time.txt
+echo "Running equiv2b_20_60"
 { time python concolic.py --coverage 20 custom.EquivTest2b 60 > equiv2b_20_60.log ; } 2>> time.txt
+echo "Running equiv2b_20_70"
 { time python concolic.py --coverage 20 custom.EquivTest2b 70 > equiv2b_20_70.log ; } 2>> time.txt
+echo "Running equiv2b_20_80"
 { time python concolic.py --coverage 20 custom.EquivTest2b 80 > equiv2b_20_80.log ; } 2>> time.txt
+echo "Running equiv2b_20_90"
 { time python concolic.py --coverage 20 custom.EquivTest2b 90 > equiv2b_20_90.log ; } 2>> time.txt
+echo "Running equiv2b_20_100"
 { time python concolic.py --coverage 20 custom.EquivTest2b 100 > equiv2b_20_100.log ; } 2>> time.txt
